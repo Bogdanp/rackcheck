4bed50ea8d15381a58c625a37cdeba6294568863
Bogdan Popa <bogdan@defn.io>
2021-09-18 08:56:23 +0300

    unicode: delay char list generation
    This greatly improves load times in racket-mode.

--- gen/unicode.rkt
+++ gen/unicode.rkt
@@ -1,9 +1,7 @@
 #lang racket/base
 
-(require (for-syntax racket/base
-                     racket/syntax
-                     syntax/parse)
-         "base.rkt")
+(require "base.rkt"
+         "syntax.rkt")
 
 (provide
  gen:unicode
@@ -14,71 +12,79 @@
  gen:unicode-symbol
  gen:unicode-separator)
 
-(begin-for-syntax
-  (define-values (letters marks numbers puncts symbols separators)
-    (for/fold ([letters    null]
-               [marks      null]
-               [numbers    null]
-               [puncts     null]
-               [symbols    null]
-               [separators null])
-              ([n (in-range 0 #x10FFFF)]
-               #:when (or (< n #xD800)
-                          (> n #xDFFF)))
-      (define char (integer->char n))
-      (define category (char-general-category char))
-      (values
-       (if (member category '(lu ll lt lm lo))
-           (cons char letters)
-           letters)
-       (if (member category '(mn mc me))
-           (cons char marks)
-           marks)
-       (if (member category '(nd nl no))
-           (cons char numbers)
-           numbers)
-       (if (member category '(pc pd ps pe pi pf po))
-           (cons char puncts)
-           puncts)
-       (if (member category '(sm sc sk so))
-           (cons char symbols)
-           symbols)
-       (if (member category '(zs zl zp))
-           (cons char separators)
-           separators)))))
+(define chars-mu (make-semaphore 1))
+(define chars
+  (make-hasheq))
 
-(define-syntax (define-codepoint-list stx)
-  (syntax-parse stx
-    [(_ which:id)
-     (with-syntax ([id (format-id #'which "char-~a-list" #'which)]
-                   [list-id (format-id #'which "~as" #'which)])
-       #'(define-syntax (id stx)
-           (datum->syntax stx (cons 'list list-id))))]))
+(define (generate-chars)
+  (for/fold ([letters    null]
+             [marks      null]
+             [numbers    null]
+             [puncts     null]
+             [symbols    null]
+             [separators null])
+            ([n (in-range 0 #x10FFFF)]
+             #:when (or (< n #xD800)
+                        (> n #xDFFF)))
+    (define char (integer->char n))
+    (define category (char-general-category char))
+    (values
+     (if (memq category '(lu ll lt lm lo))
+         (cons char letters)
+         letters)
+     (if (memq category '(mn mc me))
+         (cons char marks)
+         marks)
+     (if (memq category '(nd nl no))
+         (cons char numbers)
+         numbers)
+     (if (memq category '(pc pd ps pe pi pf po))
+         (cons char puncts)
+         puncts)
+     (if (memq category '(sm sc sk so))
+         (cons char symbols)
+         symbols)
+     (if (memq category '(zs zl zp))
+         (cons char separators)
+         separators))))
 
-(define-codepoint-list letter)
-(define-codepoint-list mark)
-(define-codepoint-list number)
-(define-codepoint-list punct)
-(define-codepoint-list symbol)
-(define-codepoint-list separator)
+(define (try-load-chars!)
+  (when (hash-empty? chars)
+    (call-with-semaphore chars-mu
+      (lambda ()
+        (when (hash-empty? chars)
+          (define-values (letters marks numbers puncts symbols separators)
+            (generate-chars))
+          (hash-set*!
+           chars
+           'letters letters
+           'marks marks
+           'numbers numbers
+           'puncts puncts
+           'symbols symbols
+           'separators separators))))))
+
+(define (get-chars category)
+  (try-load-chars!)
+  (hash-ref chars category))
 
 (define gen:unicode
   (gen:char-in 0 #x10FFFF))
 
 (define gen:unicode-letter
-  (gen:one-of (char-letter-list)))
+  (gen:delay (gen:one-of (get-chars 'letters))))
 
 (define gen:unicode-mark
-  (gen:one-of (char-mark-list)))
+  (gen:delay (gen:one-of (get-chars 'marks))))
 
 (define gen:unicode-number
-  (gen:one-of (char-number-list)))
+  (gen:delay (gen:one-of (get-chars 'numbers))))
 
 (define gen:unicode-punctuation
-  (gen:one-of (char-punct-list)))
+  (gen:delay (gen:one-of (get-chars 'puncts))))
 
 (define gen:unicode-symbol
-  (gen:one-of (char-symbol-list)))
+  (gen:delay (gen:one-of (get-chars 'symbols))))
 
 (define gen:unicode-separator
-  (gen:one-of (char-separator-list)))
+  (gen:delay (gen:one-of (get-chars 'separators))))
