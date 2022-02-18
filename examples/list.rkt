5e8da3f5ba04f8d27596a2e1c3372718b487fe4b
lemastero <adres.mistrza@gmail.com>
2022-02-18 14:31:26 +0100

    modify list example to illustrate usage of property-name

--- examples/list.rkt
+++ examples/list.rkt
@@ -1,11 +1,32 @@
 #lang racket/base
 
 (module+ test
-  (require rackcheck
+  (require racket/function
+           rackcheck
            rackunit)
 
   (define-property prop-list-reverse
     ([xs (gen:list gen:natural)])
     (check-equal? (reverse (reverse xs)) xs))
 
-  (check-property prop-list-reverse))
+  (define-property prop-list-map-identity
+    ([xs (gen:list gen:natural)])
+    (check-equal? (map identity xs) xs))
+
+  (property-name prop-list-map-identity)
+
+  (define-property prop-list-map-composition
+     ([xs (gen:list gen:natural)])
+     (let ([f (lambda (n) (* 3 (add1 n)))]
+           [g (lambda (n) (/ n 2))])
+      (check-equal? (map f (map g xs)) (map (compose f g) xs))))
+
+   (define (test-property prop)
+     (test-case (symbol->string (property-name prop))
+         (check-property prop)))
+
+  (test-property prop-list-reverse)
+
+  (test-property prop-list-map-identity)
+
+  (test-property prop-list-map-composition))
