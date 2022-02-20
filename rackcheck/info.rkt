c2b689f5fbae0777ae12617f07d0612370a72a17
lemastero <piotr.paradzinski@iohk.io>
2022-02-20 12:16:40 +0100

    [Issue 8] review: remove rackunit-lib dependency from rackcheck

--- rackcheck/info.rkt
+++ rackcheck/info.rkt
@@ -2,11 +2,9 @@
 
 (define collection "rackcheck")
 (define deps '("base"
-               "rackunit-lib"
                "rackcheck-lib"))
 (define build-deps '("racket-doc"
                      "rackunit-doc"
-                     "rackunit-lib"
                      "scribble-lib"))
 (define update-implies '("rackcheck-lib"))
 (define scribblings '(("rackcheck.scrbl")))
