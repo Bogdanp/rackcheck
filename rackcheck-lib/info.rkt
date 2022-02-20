a4a7be0669df62d222fe3e8f2962d1c84067acc0
Piotr Paradziński <piotr.paradzinski@iohk.io>
2022-02-20 19:02:49 +0100

    build: split into rackcheck and rackcheck-lib pkgs (#10)

---
+++ rackcheck-lib/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define version "2.0.1")
+(define collection "rackcheck")
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("rackunit-lib"))
--- rackcheck/info.rkt
+++ rackcheck/info.rkt
@@ -1,13 +1,10 @@
 #lang info
 
-(define version "2.0.1")
 (define collection "rackcheck")
 (define deps '("base"
-               "rackunit-lib"))
+               "rackcheck-lib"))
 (define build-deps '("racket-doc"
                      "rackunit-doc"
-                     "rackunit-lib"
                      "scribble-lib"))
+(define update-implies '("rackcheck-lib"))
 (define scribblings '(("rackcheck.scrbl")))
-(define compile-omit-paths '("examples"))
-(define test-omit-paths '("examples"))
