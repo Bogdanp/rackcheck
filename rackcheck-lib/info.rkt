12c92dfd7b8fb0081815d4d44d9ffc96e0c9cef7
Bogdan Popa <bogdan@defn.io>
2024-04-26 19:36:00 +0300

    build: bump patch version

--- rackcheck-lib/info.rkt
+++ rackcheck-lib/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "2.1")
+(define version "2.1.1")
 (define collection "rackcheck")
 (define deps '("base"
                "rackunit-lib"))
